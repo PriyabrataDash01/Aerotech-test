be5dc9a04c2fa19b6b5fca8f00e6895558acc0b1
Sreyansu S Mohanty <56058574+sreyansu@users.noreply.github.com>
2025-02-15 00:23:54 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -33,7 +33,7 @@ const About = () => {
     {
       name: "Anisha Ekka",
       branch: "Assistant Professor",
-      image: "/src/images/faculty-advisor.jpg",
+      image: "https://imgur.com/6ragQG2",
       currentRole: "Dept. of Production Engineering",
       contribution: "Guided students in pioneering innovative projects and academic excellence.",
       linkedin: "https://linkedin.com/in/anisha-ekka-28063379"
